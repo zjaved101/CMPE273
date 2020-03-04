9f33c0daff34d77f3af496798ceea88de2bf2b04
Zan Javed <{ID}+{username}@users.noreply.github.com>
2020-03-03 20:06:48 -0800

    allowed client id to be anything

--- assignments/assignment1/tcp/tcp_client.py
+++ assignments/assignment1/tcp/tcp_client.py
@@ -10,26 +10,29 @@ MESSAGE = "ping"
 
 async def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('id', type=int, help='Client ID')
+    parser.add_argument('id', help='Client ID')
     parser.add_argument('delay', type=int, help='Delay between printing messages')
     parser.add_argument('ping', type=int, help='Number of times to send the message ping to server')
     args = parser.parse_args()
 
     print("Press Ctrl + c to quit client")
     try:
+        # print("Opening Connection to server")
+        reader, writer = await asyncio.open_connection(TCP_IP, TCP_PORT)
+        # print('connected to server')
         for i in range(0, args.ping):
-            reader, writer = await asyncio.open_connection(TCP_IP, TCP_PORT)
+            # reader, writer = await asyncio.open_connection(TCP_IP, TCP_PORT)
             message = "%s:%s" % (args.id, MESSAGE)
 
             # delay message only if user provided seconds greater than 1
             if args.delay > 1:
                 time.sleep(args.delay)
 
-            print("Sending data: %s" % message)
+            print("Sending data: %s" % MESSAGE)
             writer.write(message.encode())
             data = await reader.read(BUFFER_SIZE)
             print("Received data:%s" % data.decode())
-            writer.close()
+            # writer.close()
     except KeyboardInterrupt:
         print("Client closed...")
 
