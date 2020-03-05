284b280fb66b8e4c3331ee32cf9b3633917973b5
Zan Javed <zjaved101@users.noreply.github.com>
2020-03-04 19:55:51 -0800

    Assignment1 (#1)
    * tcp and udp client and servers connecting to each other
    
    * allowed client id to be anything
    
    * handles client data until no more data left
    
    * outputs
    
    * cleaned up code
    
    * removed old logs
    
    * fixed type
    
    * outputs
    
    * cleaned up code

---
+++ assignments/assignment1/tcp/tcp_server.py
@@ -0,0 +1,33 @@
+import socket
+import asyncio
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5000
+BUFFER_SIZE = 1024
+
+async def handleClient(reader, writer):
+    data = await reader.read(BUFFER_SIZE)
+    print("Connected Client: %s" % (data.decode().split(':')[0]))
+
+    while data:
+        message = data.decode()
+        print('Received data: %s' % message)
+
+        writer.write("pong".encode())
+        await writer.drain()
+        data = await reader.read(BUFFER_SIZE)
+
+    writer.close()
+
+async def main():
+    print("Press Ctrl + c to exit this server...")
+    server = await asyncio.start_server(handleClient, TCP_IP, TCP_PORT)
+
+    async with server:
+        await server.serve_forever()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt as e:
+        print(e)
