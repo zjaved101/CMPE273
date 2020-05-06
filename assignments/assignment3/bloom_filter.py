1b823777bc9d750327ae22e611bfcffc168de933
Zan Javed <{ID}+{username}@users.noreply.github.com>
2020-05-05 19:26:37 -0700

    changed how the loop works

--- assignments/assignment3/bloom_filter.py
+++ assignments/assignment3/bloom_filter.py
@@ -7,24 +7,14 @@ class BloomFilter():
         self.list = [0 for i in range(0, self.size)]
 
     def add(self, key):
-        count = 0
-        while True:
-            count += 1
-            index = int(hashlib.md5(key.encode() + str(count).encode()).hexdigest(), 16) % self.size
+        for i in range(0, self.hash):
+            index = int(hashlib.md5(key.encode() + str(i).encode()).hexdigest(), 16) % self.size
             self.list[index] = 1
 
-            if count >= self.hash:
-                break
-
     def is_member(self, key):
-        count = 0
-        while True:
-            count += 1
-            index = int(hashlib.md5(key.encode() + str(count).encode()).hexdigest(), 16) % self.size
-            if count >= self.hash and self.list[index] == 0:
-                break
+        for i in range(0, self.hash):
+            index = int(hashlib.md5(key.encode() + str(i).encode()).hexdigest(), 16) % self.size
+            if self.list[index] == 0:
+                return False
         
-        if count == self.hash:
-            return True
-        
-        return False
+        return True
