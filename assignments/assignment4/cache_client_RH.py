1231dceb16bac21d54eb695128bfe52d8d2d516b
Zan Javed <{ID}+{username}@users.noreply.github.com>
2020-05-06 11:15:32 -0700

    RH hashing

---
+++ assignments/assignment4/cache_client_RH.py
@@ -0,0 +1,92 @@
+import sys
+import socket
+
+from sample_data import USERS
+from server_config import NODES
+from pickle_hash import serialize_GET, serialize_PUT, serialize_DELETE 
+from node_ring_RH import NodeRing
+
+BUFFER_SIZE = 1024
+NODE_RING = NodeRing(nodes=NODES)
+
+class UDPClient():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = int(port)       
+
+    def send(self, request):
+        print('Connecting to server at {}:{}'.format(self.host, self.port))
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.sendto(request, (self.host, self.port))
+            response, ip = s.recvfrom(BUFFER_SIZE)
+            return response
+        except socket.error:
+            print("Error! {}".format(socket.error))
+            exit()
+
+def process(udp_clients):
+    hash_codes = set()
+    # PUT all users.
+    # import pdb; pdb.set_trace()
+    print('====PUT====')
+    for u in USERS:
+        data_bytes, key = serialize_PUT(u)
+        # fix_me_server_id = NODE_RING.get_node(key)
+
+        fix_me_server_id = NODE_RING.rendezvous_hash_node(key)
+
+        # fix_me_server_id = NODE_RING.consistent_hash_node(key, 2)
+        response = udp_clients[fix_me_server_id].send(data_bytes)
+        # for index in fix_me_server_id:
+        #     response = udp_clients[index].send(data_bytes)
+        #     hash_codes.add(response.decode())
+        #     print(response)
+
+        hash_codes.add(response.decode())
+        print(response)
+
+    print(f"Number of Users={len(USERS)}\nNumber of Users Cached={len(hash_codes)}")
+    
+    # TODO: PART I
+    # GET all users.
+    print('====GET====')
+    for hc in hash_codes:
+        print(hc)
+        data_bytes, key = serialize_GET(hc)
+        # fix_me_server_id = NODE_RING.get_node(key)
+
+        fix_me_server_id = NODE_RING.rendezvous_hash_node(key)
+
+        # fix_me_server_id = NODE_RING.consistent_hash_node(key, 2)
+        response = udp_clients[fix_me_server_id].send(data_bytes)
+        print(response)
+
+        # for index in fix_me_server_id:
+            # response = udp_clients[index].send(data_bytes)
+            # print(response)
+
+    # DELETE all users
+    print("====DELETE====")
+    for hc in hash_codes:
+        print(hc)
+        data_bytes, key = serialize_DELETE(hc)
+
+        # fix_me_server_id = NODE_RING.get_node(key)
+
+        fix_me_server_id = NODE_RING.rendezvous_hash_node(key)
+
+        # fix_me_server_id = NODE_RING.consistent_hash_node(key, 2)
+        response = udp_clients[fix_me_server_id].send(data_bytes)
+        print(response)
+        # for index in fix_me_server_id:
+            # response = udp_clients[index].send(data_bytes)
+            # print(response)
+
+
+if __name__ == "__main__":
+    clients = [
+        UDPClient(server['host'], server['port'])
+        for server in NODES
+    ]
+    process(clients)
---
+++ assignments/assignment4/node_ring_RH.py
@@ -0,0 +1,73 @@
+import hashlib
+from server_config import NODES
+import json
+
+class NodeRing():
+
+    def __init__(self, nodes):
+        assert len(nodes) > 0
+        self.nodes = nodes
+        # self.consistent_hash_size = 2**32
+        # self.consistent_hash_index = {}
+        # self.virtual_nodes = virtual_nodes
+        # self.virtual_nodes = {}
+        # for node in range(0,virtual_nodes):
+            # self.virtual_nodes[node % len(nodes)] = self.nodes[node % len(nodes)]
+    
+    def get_node(self, key_hex):
+        key = int(key_hex, 16)
+        node_index = key % len(self.nodes)
+        # return self.nodes[node_index]
+        return node_index
+
+    def rendezvous_hash_node(self, key_hex):
+        largest = -1
+        node_index = 0
+        for index, node in enumerate(self.nodes):
+            value = int(hashlib.md5(key_hex.encode() + str(node['port']).encode()).hexdigest(), 16)
+
+            if value > largest:
+                largest = value
+                node_index = index
+
+        # for node in self.nodes:
+        #     value = int(hashlib.md5(key_hex.encode() + str(node['port']).encode()).hexdigest(), 16)
+
+        #     if value > largest:
+        #         largest = value
+        #         node_index = node['port'] % 10 # get the last digit of port 
+        
+        return node_index
+
+    # def closestIndex(self, list, key):
+    #     return list[min(range(len(list)), key = lambda i: abs(list[i]-key))]
+
+    # def consistent_hash_node(self, key_hex, replication):
+    #     if not self.consistent_hash_index:
+    #         # for node in self.nodes:
+    #         # for node in self.virtual_nodes:
+    #         for index, node in enumerate(self.virtual_nodes):
+
+    #             # value = hash(frozenset(node.items())) % 360
+    #             # self.consistent_hash_index[value] = node['port'] % 10
+    #             # value = hash(node) % 360
+
+    #             value = hash(node) % self.consistent_hash_size
+    #             # self.consistent_hash_index[value] = self.virtual_nodes[node]['port'] % 10
+    #             self.consistent_hash_index[value] = index
+
+    #     value = hash(key_hex)
+    #     # value = int(hashlib.sha512(key_hex.encode()).hexdigest(), 16)
+    #     index = self.consistent_hash_index[self.closestIndex(list(self.consistent_hash_index.keys()), value)]
+    #     # return self.consistent_hash_index[self.closestIndex(list(self.consistent_hash_index.keys()), value)]
+    #     return [(index + i) % len(self.virtual_nodes) for i in range(0, replication)]
+
+def test():
+    ring = NodeRing(nodes=NODES, virtual_nodes=8)
+    node = ring.get_node('9ad5794ec94345c4873c4e591788743a')
+    print(node)
+    print(ring.get_node('ed9440c442632621b608521b3f2650b8'))
+
+
+# Uncomment to run the above local test via: python3 node_ring.py
+# test()
